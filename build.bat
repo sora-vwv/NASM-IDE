5b2dc1ff76b17191555b030c9120a40eb3775f07
Anton Kochergin <87330809+sora-vwv@users.noreply.github.com>
2023-09-08 17:05:27 +0400

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+color 03
+"./NASM/nasm.exe" --gprefix _ -f win32 "%1" -o "%1.o" -i "./include/"
+"./MinGW/bin/gcc.exe" "%1.o" "./NASM/macro.o" -g -o "%1.exe" -m32
+echo.Program is completed
+pause >nul
+exit 0
---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+color 03
+"%1.exe"
+echo.Program is completed
+pause >nul
+exit 0
